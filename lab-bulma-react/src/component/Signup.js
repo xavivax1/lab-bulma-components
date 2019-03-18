4cc4f02f7aa26a8c39889bb3352d444fce9fd613
xavivax1 <xavier.deviala@gmail.com>
2019-03-19 00:30:25 +0100

    a dormir!

--- lab-bulma-react/src/component/Signup.js
+++ lab-bulma-react/src/component/Signup.js
@@ -9,14 +9,21 @@ class SignUp extends Component {
       <div>
         <NavBar></NavBar>
         <form>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input class="button" type="submit" value="Submit input"></input>
-          <CoolButton></CoolButton>
+          <input type="text">Name</input>
+          <input type="email">email</input>
+          <input type="password">password</input>
+          <input class="button" type="submit"></input>
         </form>
       </div>
     )
   }
 }
 export default CoolButton;
+
+/*
+<form action="/action_page.php">
+  First name: <input type="text" name="fname"><br>
+  Last name: <input type="text" name="lname"><br>
+  <input type="submit" value="Submit">
+</form>
+*/
